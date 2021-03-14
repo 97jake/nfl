65c6af4fa29364331e8e2ed8bf7921aecf2f62a0
Jake Snow <97jake@gmail.com>
2021-03-14 07:03:06 -0600

    Testing out histogram
    Most important change is the addition of __pychache__ to .gitignore

--- DataVisualization.py
+++ DataVisualization.py
@@ -5,5 +5,18 @@ Created on Sat Mar 13 22:45:42 2021
 @author: 97jak
 """
 
+import OvertimeAnalysis as oa
+from matplotlib import pyplot as plt
+import numpy as np
 
+info = oa.get_drive_info('BAL',2019)
 
+yards = []
+
+for item in info:
+    yards.append(item[2])
+
+yards = np.array(yards)
+
+plt.hist(yards,10)
+plt.show()
